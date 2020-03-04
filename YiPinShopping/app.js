61549a08c74af96d4cbd26c4dfea353b59d44440
newpageone <liu6email@163.com>
2020-03-04 23:11:19 +0800

    change style and something

--- YiPinShopping/app.js
+++ YiPinShopping/app.js
@@ -3,10 +3,10 @@ App({
   addToCart(obj) {
     // 1.判断是否已经添加进来
     const oldInfo = this.globalData.cartList.find((item) => item.id === obj.id)
-    if (oldInfo) {
+    if (oldInfo) { 
       oldInfo.count += 1
     } else {
-      obj.count = 1
+      obj.count = 1 
       obj.checked = true
       this.globalData.cartList.push(obj)
     }
